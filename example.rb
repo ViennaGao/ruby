ee78d9725bf8031bb36bc0f1676f683315fd7e9d
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-11-13 22:08:07 +0000

    Edit Ruby script to cause some merging fun

--- example.rb
+++ example.rb
@@ -1,3 +1,9 @@
+#
 # Just a placeholder script
+# ...which now has been edited.
+#
 
 puts "Yay, I have managed to clone a remote repository!"
+puts "Yay, I have managed to pull from another remote repository!"
+puts "This was a local change"
+
